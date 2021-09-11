7c26bfbd139c16f34519836ee4a4d999afbeedd4
Chris Werkheiser <chriswerkheiser@Chriss-MacBook-Pro.local>
2021-09-10 18:38:00 -0700

    format doc

--- script.js
+++ script.js
@@ -17,13 +17,13 @@
 		const keyCode = e.keyCode,
 			keyElement = document.querySelector(`div[data-key="${keyCode}"]`);
 
-		if(!keyElement) return;
+		if (!keyElement) return;
 
 		const audioElement = document.querySelector(`audio[data-key="${keyCode}"]`);
 		audioElement.currentTime = 0;
 		audioElement.play();
 
-		switch(keyCode) {
+		switch (keyCode) {
 			case 69:
 			case 82:
 				animateCrashOrRide();
@@ -37,19 +37,19 @@
 	};
 
 	const removeCrashRideTransition = e => {
-		if(e.propertyName !== 'transform') return;
+		if (e.propertyName !== 'transform') return;
 
 		e.target.style.transform = 'rotate(-7.2deg) scale(1.5)';
 	};
 
 	const removeHiHatTopTransition = e => {
-		if(e.propertyName !== 'top') return;
+		if (e.propertyName !== 'top') return;
 
 		e.target.style.top = '166px';
-	};	
+	};
 
 	const removeKeyTransition = e => {
-		if(e.propertyName !== 'transform') return;
+		if (e.propertyName !== 'transform') return;
 
 		e.target.classList.remove(playingClass)
 	};
